cecdd06145f7a36039f7089b56ebe38c03bd6eeb
rafael vincent <d749107@gmail.com>
2025-09-04 20:27:42 +0800

    hey

--- src/utils/searchUtils.js
+++ src/utils/searchUtils.js
@@ -1,14 +1,14 @@
 export const handleSearch = async (text, setVideoId) => {
   try {
     const response = await fetch(
-      `http://localhost:5000/search?q=${encodeURIComponent(text)}`
+      `https://secretnotesapp.onrender.com/search?q=${encodeURIComponent(text)}`
     );
     if (!response.ok) throw new Error("Failed to fetch from backend");
 
     const data = await response.json();
 
     if (data.results && data.results.length > 0) {
-      setVideoId(data.results[0]); // 👈 pick the first video ID
+      setVideoId(data.results[0]); // pick the first video ID
     } else {
       console.warn("No video IDs returned");
     }
