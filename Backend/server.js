c4910092896279b7fb9cc9e99c06b583a80749a5
rafael vincent <d749107@gmail.com>
2025-12-18 15:53:49 +0800

    Change server port from 5000 to 5001

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ import { both } from "./controller/getSongandLyrics.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 
